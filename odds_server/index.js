7e41f01350ace6277fb1b5623b79196b582682d5
Martin-Andonov <85078216+Martin-Andonov@users.noreply.github.com>
2024-01-14 23:29:18 +0200

    added odds server with api requests and client server file

---
+++ odds_server/index.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+sports = ['soccer_uefa_europa_league','soccer_uefa_euro_qualification','soccer_uefa_champs_league','icehockey_sweden_hockey_league','icehockey_nhl','basketball_nba','americanfootball_nfl'];
+
+//https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds?regions=us&apiKey=YOUR_API_KEY
+//87d26f4d65bad7109668c0cadf6b456b
+for(let i = 0; i < 7; i++)
+{
+    console.log(sports[i]);
+axios.get(`https://api.the-odds-api.com/v4/sports/${sports[i]}/odds/?apiKey=87d26f4d65bad7109668c0cadf6b456b&regions=eu&markets=h2h`)
+.then(response => {
+    console.log('API Response:',response.data.values());
+
+    for( const val of response.data.values()){
+       for(const vall of  val["bookmakers"].values())
+       {
+        if(vall["key"] == "onexbet")
+        {
+            console.log(vall["markets"][0]["outcomes"]);
+        }
+       }
+    }  
+  })
+  .catch(error => {
+    console.error('Error making API request:', error.message);
+  });
+}
